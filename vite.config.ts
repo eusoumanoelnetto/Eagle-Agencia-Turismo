3e170e8b6113ca6dc9fdf0fc1cc41e8a4da063ac
eusoumanoelnetto <manoelccoelho@gmail.com>
2026-02-20 01:26:30 -0300

    feat: atualiza base do Vite para novo caminho e adiciona arquivo CNAME

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
-      base: '/eagle-agencia-digital/',
+      base: '/turismo/',
       server: {
         port: 3000,
         host: '0.0.0.0',
